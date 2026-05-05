6c4b99e6520874f1521081b689236708e54ec73e
xiaoz <xiaoz93@outlook.com>
2026-05-05 08:44:11 +0800

    修改宽度

--- components/PopupLayout.vue
+++ components/PopupLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-[600px] w-[380px] flex-col overflow-hidden bg-slate-50 text-slate-900">
+  <div class="flex h-[600px] w-[400px] flex-col overflow-hidden bg-slate-50 text-slate-900">
     <slot />
   </div>
 </template>
